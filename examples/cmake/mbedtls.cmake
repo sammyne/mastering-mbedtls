5a2db05a612fe7e0c6b51200e484c74b91eca5d6
sammyne <xiangminli@alumni.sjtu.edu.cn>
2021-09-01 09:01:08 +0000

    fix(examples/cmake): fix conflicting CONFIGURE_COMMAND and CMAKE_ARGS

--- examples/cmake/mbedtls.cmake
+++ examples/cmake/mbedtls.cmake
@@ -1,12 +1,14 @@
 include(ExternalProject)
 
+# CONFIGURE_COMMAND and CMAKE_ARGS is conflicting, cannot be specifed both
 ExternalProject_Add(mbedtls 
   PREFIX mbedtls
   GIT_REPOSITORY https://gitee.com/sammyne/mbedtls.git 
   GIT_TAG v2.27.0 
   INSTALL_DIR ${PROJECT_SOURCE_DIR}/third-party/_mbedtls 
-  CONFIGURE_COMMAND bash ${PROJECT_SOURCE_DIR}/scripts/patch_mbedtls.sh <SOURCE_DIR>
-  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${PROJECT_SOURCE_DIR}/third-party/_mbedtls -DENABLE_TESTING=Off
+  CONFIGURE_COMMAND bash ${PROJECT_SOURCE_DIR}/scripts/patch_mbedtls.sh <SOURCE_DIR> &&
+    cmake . -DCMAKE_INSTALL_PREFIX=${PROJECT_SOURCE_DIR}/third-party/_mbedtls -DENABLE_TESTING=Off
+  BUILD_COMMAND make -j
   BUILD_IN_SOURCE 1)
 
 ExternalProject_Get_Property(mbedtls INSTALL_DIR)
@@ -15,15 +17,19 @@ ExternalProject_Get_Property(mbedtls INSTALL_DIR)
 set(MBEDTLS_INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
 set(MBEDTLS_LINK_DIRECTORIES ${INSTALL_DIR}/lib)
 
+# create the ${INSTALL_DIR}/include directory
+file(MAKE_DIRECTORY ${INSTALL_DIR}/include)
+
 add_library(mbedtls-crypto STATIC IMPORTED GLOBAL)
+add_dependencies(mbedtls-crypto mbedtls)
 set_property(TARGET mbedtls-crypto PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/libmbedcrypto.a)
 set_property(TARGET mbedtls-crypto PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
 
-add_library(mbedtls-tls STATIC IMPORTED GLOBAL)
-set_property(TARGET mbedtls-tls PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/libmbedtls.a)
-set_property(TARGET mbedtls-tls PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
+#add_library(mbedtls-tls STATIC IMPORTED GLOBAL)
+#set_property(TARGET mbedtls-tls PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/libmbedtls.a)
+#set_property(TARGET mbedtls-tls PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
 
-add_library(mbedtls-x509 STATIC IMPORTED GLOBAL)
-set_property(TARGET mbedtls-x509 PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/libmbedx509.a)
-set_property(TARGET mbedtls-x509 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
+#add_library(mbedtls-x509 STATIC IMPORTED GLOBAL)
+#set_property(TARGET mbedtls-x509 PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/libmbedx509.a)
+#set_property(TARGET mbedtls-x509 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
 
